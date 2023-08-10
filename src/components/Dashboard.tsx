a420a6ad2f5d9183f4cf3442edf4a6ea86dcfa58
Cyber Superior <cybersuperior1@gmail.com>
2023-08-09 21:18:22 -0500

    Add dashboard with grid, typography and cards

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent, Grid, Typography } from "@mui/material"
+
+export const Dashboard = () => {
+    return (
+    <Grid container spacing={8}>
+        <Grid item xs={6}>
+            
+    <Card sx={{ boxShadow: 3 }} >
+        <CardContent>
+
+        <Typography variant={"h6"}>This is the left side of the page in an h6 header</Typography> 
+        </CardContent>
+           </Card>
+        </Grid>
+        <Grid item xs={6}>
+        <Typography variant={"h5"}> This is the right side of the page in an h1 header</Typography>
+        </Grid>
+    </Grid>)
+}
